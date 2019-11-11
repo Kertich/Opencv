9548e95504db9c70a1e7e024d4c2f9ffa33d7d36
Kertich <kipsangkertich@gmail.com>
2019-11-11 19:36:38 +0300

    modified

--- draw_cirles.py
+++ draw_cirles.py
@@ -27,3 +27,13 @@ for r in range(0, 175, 25):
 cv2.imshow("concentric circles", canvas)
 cv2.waitKey(0)
 
+# generate random radius, center point, color
+# draw circles in for loop
+canvas = np.zeros((300, 300, 3), dtype = "uint8")
+for i in range(0,25):
+    radius = np.random.randint(5, high = 200)
+    color = np.random.randint(0, high=256, size=(3,)).tolist()
+    pt = np.random.randint(0, high = 300, size = (2,))
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+cv2.imshow("canvas", canvas)
+cv2.waitKey(0)
